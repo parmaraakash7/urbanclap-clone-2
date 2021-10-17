cb0018f3f549a76a634fdfd43b1ab0fa659af1a0
parmaraakash7 <parmaraakash783@gmail.com>
2021-10-17 14:26:36 +0530

    Added closest

--- src/main/java/com/example/demo/utils/ConnectionModel.java
+++ src/main/java/com/example/demo/utils/ConnectionModel.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionModel {
+	static public int FIVE_KM = 2000;
 	static public Connection getConnection() throws SQLException {
 		Connection c = null;
 		c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/urbanclap-clone2","postgres", "aakash@123");
